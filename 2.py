98b503edc6e61408dca26f1baa10663c3d68866e
sklyarvik <sklyrenko19998@mail.ru>
2024-01-17 22:47:01 +0300

    rename

--- 2.py
+++ 2.py
@@ -1,12 +1,23 @@
-import re
-def convert_number(text):
-    pattern = r'(\d{2,4}\\\d{2,5})'
-    result = re.findall(pattern, text)
+def add_atms(n, k, distances):
+    # Проверка на коректность
+    if n <= 0 or k < 0 or len(distances) != n - 1:
+        return "Некорректные входные данные"
 
-    for num in result:
-        parts = num.split('\\')
-        good_number = parts[0].zfill(4) + '\\' + parts[1].zfill(5)
-        print(good_number)
+    # Сортировка расстояния в порядке убывания
+    distances.sort(reverse=True)
 
-text = "Некоторые особенные номера: 5467\\456, 405\\549"
-convert_number(text)
+    # Добавляет k банкоматов делая меньше максимальное расстояние
+    for i in range(k):
+        max_distance = distances.pop(0)
+        new_distance = max_distance // 2
+        distances.extend([new_distance, max_distance - new_distance])
+
+    # Сортируем  по возрастанию
+    distances.sort()
+
+    return distances
+n = 5
+k = 3
+distances = [100, 30, 20, 80]
+result = add_atms(n, k, distances)
+print(result)
