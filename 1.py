de3d36fa3133f9e664b0585c25ed38b67b0e2a60
sklyarvik <sklyrenko19998@mail.ru>
2024-01-18 12:45:39 +0300

    init

--- 1.py
+++ 1.py
@@ -1,13 +1,12 @@
-from functools import cmp_to_key
-def compare(a, b):
-    return (int(b + a) - int(a + b))
+import re
+def convert_number(text):
+    pattern = r'(\d{2,4}\\\d{2,5})'
+    result = re.findall(pattern, text)
 
+    for num in result:
+        parts = num.split('\\')
+        good_number = parts[0].zfill(4) + '\\' + parts[1].zfill(5)
+        print(good_number)
 
-def largest_number(strings):
-    sorted_strings = sorted(strings, key=cmp_to_key(compare))
-    max_number = ''.join(sorted_strings)
-    max_number = str(int(max_number))
-
-    return max_number
-lst = ['41', '4', '005', '89']
-print(largest_number(lst))
+text = "Некоторые особенные номера: 5467\\456, 405\\549"
+convert_number(text)
